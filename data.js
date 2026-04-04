e3694db508969fe0dcbc357eca7df9f880ca45ba
Bruno <brunovonkoln@gmail.com>
2026-04-04 00:46:03 -0300

    .

--- data.js
+++ data.js
@@ -1,63 +1,74 @@
-export default  [  
+export default [
   {
-    title:"Aula 0 Boas Vindas",
+    title: "Aula 0 Boas Vindas",
     artist: "Módulo 00",
     cover: "1.jpg",
     file: "1.mp3",
-  },  
+  }/*, // primeiro item do array
+
   {
-    title:"2 -",
+    title: "2 -",
     artist: "",
     cover: "1.jpeg",
     file: "2.mp3",
-  },  
+  }, // segundo item do array
+
   {
-    title:"3 -",
+    title: "3 -",
     artist: "",
     cover: "1.jpeg",
     file: "3.mp3",
-  },  
+  },
+
   {
-    title:"4 -",
+    title: "4 -",
     artist: "Remix Taos Elieri",
     cover: "1.jpeg",
     file: "4.mp3",
-  },  
+  }, // exemplo de comentário adicional
+
   {
-    title:"5 -",
+    title: "5 -",
     artist: "",
     cover: "1.jpeg",
     file: "5.mp3",
-  },  
+  },
+
   {
-    title:"6 -",
+    title: "6 -",
     artist: "",
     cover: "1.jpeg",
     file: "6.mp3",
-  },  
+  },
+
   {
-    title:"7 - ",
+    title: "7 - ",
     artist: "",
     cover: "1.jpeg",
     file: "7.mp3",
-  },  
+  },
+
   {
-    title:"8 -",
+    title: "8 -",
     artist: "Taos Elieri",
     cover: "1.jpeg",
     file: "8.mp3",
-  },  
+  },
+
   {
-    title:"9 -",
+    title: "9 -",
     artist: "",
     cover: "1.jpeg",
     file: "9.mp3",
-  },  
+  },
+
   {
-    title:"10 -",
+    title: "10 -",
     artist: "",
     cover: "1.jpeg",
     file: "10.mp3",
-  }
- ];
+  },
+
+  // fim do array de objetos, sem caracteres inválidos */
+];
 
