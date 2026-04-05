f517b3457b820eab0b9c7fc01b39b3de537f6877
Bruno <brunovonkoln@gmail.com>
2026-04-05 17:56:48 -0300

    ...

--- data.js
+++ data.js
@@ -1,83 +1,85 @@
+const DEFAULT_COVER = "0.jpg";
+
 export default [
   {
     title: "Boas Vindas",
     artist: "-",
-    cover: "0.jpg",
+    cover: DEFAULT_COVER,
     file: "0.mp3",
   },
 
   {
     title: "Aula 1 - Desmistificando o Leilão",
     artist: "Módulo 01 - Noções gerais sobre os leilões",
-    cover: "0.jpg",
+    cover: DEFAULT_COVER,
     file: "1.mp3",
   }, // segundo item do array
 
   {
     title: "Aula 2 - Por que o imóvel vai a leilão e modalidades",
     artist: "Módulo 01 - Noções gerais sobre os leilões",
-    cover: "0.jpg",
+    cover: DEFAULT_COVER,
     file: "2.mp3",
   },
 
   {
     title: "Aula 3 - Tipos de Leilão, Online, Presencial e Híbrido",
     artist: "Módulo 01 - Noções gerais sobre os leilões",
-    cover: "0.jpg",
+    cover: DEFAULT_COVER,
     file: "3.mp3",
   }, // exemplo de comentário adicional
 
   {
     title: "Aula 4 - Quem pode participar dos leilões",
     artist: "Módulo 01 - Noções gerais sobre os leilões",
-    cover: "0.jpg",
+    cover: DEFAULT_COVER,
     file: "4.mp3",
   },
 
   {
     title: "Aula 5 - Matrícula do Imóvel",
     artist: "Módulo 01 - Noções gerais sobre os leilões",
-    cover: "0.jpg",
+    cover: DEFAULT_COVER,
     file: "5.mp3",
   },
 
   {
     title: "Aula 6 - Bem de Família ",
     artist: "Módulo 01 - Noções gerais sobre os leilões",
-    cover: "1.jpg",
+    cover: DEFAULT_COVER,
     file: "6.mp3",
   },
 
   {
     title: "Aula 7 - Leiloeiros Oficiais",
     artist: "Módulo 01 - Noções gerais sobre os leilões",
-    cover: "1.jpg",
+    cover: DEFAULT_COVER,
     file: "7.mp3",
   },
 
   {
     title: "Aula 8 - Edital de Leilão",
     artist: "Módulo 01 - Noções gerais sobre os leilões",
-    cover: "1.jpg",
+    cover: DEFAULT_COVER,
     file: "8.mp3",
   },
 
   {
     title: "Aula 9 - Primeira e Segunda Praça",
     artist: "Módulo 01 - Noções gerais sobre os leilões",
-    cover: "1.jpg",
+    cover: DEFAULT_COVER,
     file: "9.mp3",
   },
  {
     title: "Aula 10 - Ferramenta de avaliação",
     artist: "Módulo 01 - Noções gerais sobre os leilões",
-    cover: "1.jpg",
+    cover: DEFAULT_COVER,
     file: "10.mp3",
   },
   {
     title: "Aula 11 - Calculadora de Leilão",
     artist: "Módulo 01 - Noções gerais sobre os leilões",
-    cover: "1.jpg",
+    cover: DEFAULT_COVER,
     file: "11.mp3",
   }
  /* */
